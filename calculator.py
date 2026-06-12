c1319d723c763f937f80ff109625cff726bae56f
ChettVarsha <chettyvarsha18@gmail.com>
2026-06-12 16:50:38 +0530

    created 3 variable

--- calculator.py
+++ calculator.py
@@ -1,11 +1,11 @@
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
-
-print("Sum =", a + b)
-print("Sub =", a - b)
-print("Mul =", a * b)
+c = int(input("Enter third number: "))
+print("Sum =", a + b + c)
+print("Sub =", a - b - c)
+print("Mul =", a * b * c)
 
 if b != 0:
     print("Div =", a / b)
 else:
-    print("Division by zero is not allowed")
+    print("Division by zero is not allowed.")
